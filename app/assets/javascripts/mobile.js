f4db60e5a45ca4537308b170def5901e3cdb0aa0
shk33 <m.coronel.seg@gmail.com>
2015-01-31 03:26:50 -0600

    Add test to mespeak

--- app/assets/javascripts/mobile.js
+++ app/assets/javascripts/mobile.js
@@ -1,9 +1,14 @@
 $(document).ready(function($) {
   
+  /* Cargando meSpeak */
+  meSpeak.loadConfig("mespeak/mespeak_config.json");
+  meSpeak.loadVoice('mespeak/voices/es.json');
+  meSpeak.speak('Me gustan las chicas grandes');
+
   var myElement = document.getElementById('flow-element');
   var hammertime = new Hammer(myElement);
   hammertime.on('tap', function(ev) {
       console.log(ev);
   });
-  
+
 });
