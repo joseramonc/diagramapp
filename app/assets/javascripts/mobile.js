8cce7c4b7044a8de086a334992e5e931ef20e349
joseramonc <joserracamacho@gmail.com>
2015-01-31 16:50:53 -0600

    fix mobile

--- app/assets/javascripts/mobile.js
+++ app/assets/javascripts/mobile.js
@@ -1,5 +1,5 @@
 $(document).ready(function($) {
-  
+
   /* Cargando meSpeak */
   meSpeak.loadConfig("/mespeak/mespeak_config.json");
   meSpeak.loadVoice('/mespeak/voices/es-la.json');
@@ -16,107 +16,110 @@ $(document).ready(function($) {
   meSpeak.speak(instrucciones, { "speed": "130", "amplitude": "120", "wordgap": "2", "variant": "m5"});
 
   var myElement = document.getElementById('flow-element');
-  var hammertime = new Hammer(myElement);
+  if (myElement != undefined) {
 
-  /*
-  * Dice la información del nodo
-  */
-  hammertime.on('tap', function(ev) {
-    console.log(ev.tapCount);
-    node_content = $('#node-content').html();
-    contenido = 'El Contenido del nodo es , '+ node_content;
-    meSpeak.speak(contenido);
-  });
+    var hammertime = new Hammer(myElement);
 
-  /*
-  * Dice la información de ayuda del profesor
-  */
-  var mc = new Hammer.Manager(myElement, {});
-  mc.add( new Hammer.Tap({ event: 'doubletap', taps: 1, interval: 10000 }) );
-  hammertime.on('pinch', function(ev) {
-      nodeHelp = $('#node-help').html();
-      meSpeak.speak('Información Adicional, '+ nodeHelp);
-  });
+    /*
+    * Dice la información del nodo
+    */
+    hammertime.on('tap', function(ev) {
+      console.log(ev.tapCount);
+      node_content = $('#node-content').html();
+      contenido = 'El Contenido del nodo es , '+ node_content;
+      meSpeak.speak(contenido);
+    });
 
-  /*
-  * Mostrar el siguiente o ir a true
-  */
-  hammertime.on('swipeleft', function(ev) {
-      //console.log(ev);
-      //0 = false, 1 = true
-      element = $('#flow-element');
+    /*
+    * Dice la información de ayuda del profesor
+    */
+    var mc = new Hammer.Manager(myElement, {});
+    mc.add( new Hammer.Tap({ event: 'doubletap', taps: 1, interval: 10000 }) );
+    hammertime.on('pinch', function(ev) {
+        nodeHelp = $('#node-help').html();
+        meSpeak.speak('Información Adicional, '+ nodeHelp);
+    });
 
-      // url = "node/:id/next?=0"
-      // node/4
-      id = element.data('id');
-      url = "/nodes/" + id +"/next";
-      if(element.data('type') === 'Content'){
-        url += "?condition=1"
-      }
+    /*
+    * Mostrar el siguiente o ir a true
+    */
+    hammertime.on('swipeleft', function(ev) {
+        //console.log(ev);
+        //0 = false, 1 = true
+        element = $('#flow-element');
 
-      $.ajax({
-        url: url,
-        type: 'GET',
-        data: { },
-      })
-      .done(function(data) {
-        //Actualizar data attributes DONE
-        element   = $('#flow-element');
-        flow_shape = $('#flow-shape');
-        node_content = $('#node-content').html(data.text);
-
-        element.data('id',   data.id);
-        element.data('type', data.type);
-        console.log(data);
-        //console.log(data);
-        //Y renderizar el dibujo
-        shapeClass = "";
-        tipoNodo   = "";
-        if(data.position == 'initial' || data.position == 'final'){
-          if (data.position == 'initial') {
-            tipoNodo = "Inicio";
-          }else{
-            tipoNodo = "Final";
-          }
-          shapeClass = 'oval';
-        }
-        else if(data.type == 'Condition'){
-          tipoNodo   = "Condición";
-          shapeClass = 'rombo';
-          }
-        else{
-          tipoNodo   = "Actividad";
-          shapeClass = 'rectangle';
+        // url = "node/:id/next?=0"
+        // node/4
+        id = element.data('id');
+        url = "/nodes/" + id +"/next";
+        if(element.data('type') === 'Content'){
+          url += "?condition=1"
         }
 
-        text = $(element.find('h4')[0]);
-        text.html(data.text);
-        flow_shape.attr('class', shapeClass);
+        $.ajax({
+          url: url,
+          type: 'GET',
+          data: { },
+        })
+        .done(function(data) {
+          //Actualizar data attributes DONE
+          element   = $('#flow-element');
+          flow_shape = $('#flow-shape');
+          node_content = $('#node-content').html(data.text);
 
-        node_content = $('#node-content').html();      
-        instrucciones = 'Tipo de nodo , '+ tipoNodo;
-        instrucciones += ', Contenido , '+ node_content;
-        meSpeak.speak(instrucciones, { "speed": "130", "amplitude": "120", "wordgap": "2", "variant": "m5"});
-      });
-  });
+          element.data('id',   data.id);
+          element.data('type', data.type);
+          console.log(data);
+          //console.log(data);
+          //Y renderizar el dibujo
+          shapeClass = "";
+          tipoNodo   = "";
+          if(data.position == 'initial' || data.position == 'final'){
+            if (data.position == 'initial') {
+              tipoNodo = "Inicio";
+            }else{
+              tipoNodo = "Final";
+            }
+            shapeClass = 'oval';
+          }
+          else if(data.type == 'Condition'){
+            tipoNodo   = "Condición";
+            shapeClass = 'rombo';
+            }
+          else{
+            tipoNodo   = "Actividad";
+            shapeClass = 'rectangle';
+          }
 
-  /*
-  * Ir a false
-  */
-  hammertime.on('press', function(ev) {
-      //console.log(ev);
-      //0 = false, 1 = true
-      url = "node/:id/next?=0"
-      $.ajax({
-        url: url,
-        type: 'GET',
-        data: {param1: 'value1'},
-      })
-      .done(function() {
-        console.log("success");
-      });
-      
-      meSpeak.speak('Me gustan las chicas grandes');
-  });
+          text = $(element.find('h4')[0]);
+          text.html(data.text);
+          flow_shape.attr('class', shapeClass);
+
+          node_content = $('#node-content').html();
+          instrucciones = 'Tipo de nodo , '+ tipoNodo;
+          instrucciones += ', Contenido , '+ node_content;
+          meSpeak.speak(instrucciones, { "speed": "130", "amplitude": "120", "wordgap": "2", "variant": "m5"});
+        });
+    });
+
+    /*
+    * Ir a false
+    */
+    hammertime.on('press', function(ev) {
+        //console.log(ev);
+        //0 = false, 1 = true
+        url = "node/:id/next?=0"
+        $.ajax({
+          url: url,
+          type: 'GET',
+          data: {param1: 'value1'},
+        })
+        .done(function() {
+          console.log("success");
+        });
+
+        meSpeak.speak('Me gustan las chicas grandes');
+    });
+  }
 
 });
